b4d0b11a580621f88e964da434643cee69434694
abuyako <abuyako@gmail.com>
2020-04-09 17:22:52 +0300

    fixng navigation

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-let mainNav = document.getElementById('myTopnav');
+let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
